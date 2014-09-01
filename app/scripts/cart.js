f42b3d38db1bfa5e02105cade90f1256ff2f587a
Pollyanna <2639436413@qq.com>
2014-09-01 10:58:51 +0800

    购物车显示

--- app/scripts/cart.js
+++ app/scripts/cart.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
     Cart.refresh_the_cart();
-    var item_detail = $();
+    var cart = Cart.get_cart(), item_detail;
+    _(_(cart.bought_items).keys()).each(function(item_name){
+        item_detail = item_detail_generator(cart.bought_items[item_name]);
+        item_detail.appendTo($('table'));
+    });
+
 });
 
-//<tr>
-//    <td>饮料</td>
-//    <td>可口可乐</td>
-//    <td>3</td>
-//    <td>瓶</td>
-//    <td>
-//        <div class="btn-group">
-//            <button type="button" class="btn btn-default">-</button>
-//            <button type="button" class="btn btn-default">3</button>
-//            <button type="button" class="btn btn-default">+</button>
-//        </div>
-//    </td>
-//    <td>9</td>
-//</tr>
+
+var item_detail_generator = function(item){
+    var item_detail =$('<tr><td>'+item.kind+'</td><td>'
+        +item.name+'</td><td>'+
+        item.price+'</td><td>'+
+        item.unit+'</td><td><div class="btn-group">' +
+        '<button type="button" class="btn btn-default">-</button>' +
+        '<button type="button" class="btn btn-default">'+item.count+'</button>' +
+        '<button type="button" class="btn btn-default">+</button></div></td>'+
+        '<td>'+(item.price*item.count)+'</td>'
+    );
+    return item_detail;
+};
